c08cc884c02ae9e3b1f82277489d7c9358f81b1f
m4t1ce <m4t1ce@gmail.com>
2021-02-13 21:16:08 +0100

    updated diplay home cost

--- nanoComponents/DisplayModalCard.tsx
+++ nanoComponents/DisplayModalCard.tsx
@@ -7,17 +7,22 @@ export const DisplayModalCard = (props) => {
 			<p>
 				{props.cardData.id}. {props.cardData.name}
 			</p>
-			{props.cardData.price !== 0 ? <p>cena:{props.cardData.price}</p> : null}
+			{props.cardData.price !== 0 ? <p>cena: {props.cardData.price}</p> : null}
 			{/* {console.log(props.cardData.tax)} */}
 			{props.cardData.tax === undefined ||
 			props.cardData.tax === null ? null : props.cardData.tax.length === 1 ? (
-				<p>{props.cardData.tax[0]}</p>
+				<>
+					<p>{props.cardData.tax[0]}</p>
+				</>
 			) : (
-				<ol>
-					{props.cardData.tax.map((x) => {
-						return <li>{x}</li>;
-					})}
-				</ol>
+				<>
+					<p>Cena domq: {props.cardData.homeCost}</p>
+					<ol>
+						{props.cardData.tax.map((x) => {
+							return <li>{x}</li>;
+						})}
+					</ol>
+				</>
 			)}
 		</div>
 	);
